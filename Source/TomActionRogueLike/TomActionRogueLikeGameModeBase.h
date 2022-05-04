c70cb4ae3b78d69625bb504526653fca8d15ca19
HeavenScream <carlos.hs93@live.com.mx>
2022-05-04 17:33:44 -0500

    Base Project c++
    LETS GO!!!!

---
+++ Source/TomActionRogueLike/TomActionRogueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TomActionRogueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMACTIONROGUELIKE_API ATomActionRogueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
